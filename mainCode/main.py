469fd058f0fab568b13f8190a3a62e9e89ec3c4e
NghiTran <trantrung.nghi01@gmail.com>
2024-10-08 11:24:56 -0400

    import additional function from servo file to main

--- mainCode/main.py
+++ mainCode/main.py
@@ -1,3 +1,4 @@
 from LED_RGB import welcomeColor, warningColor, dangerColor, CorrectColor
 from buzzer import buzzWelcome, buzzCorrect, buzzWrong, buzzDanger
 from motionDetect import motionDetect
+from servoMotor import open_door, close_door
